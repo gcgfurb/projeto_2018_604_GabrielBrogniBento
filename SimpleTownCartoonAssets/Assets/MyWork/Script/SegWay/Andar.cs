7438c7e44c3a35fc4d4b8d69b69e766d52d77cc5
Gabriel-brogni-bento <gabrielbento98@gmail.com>
2018-11-23 17:40:16 -0200

    Integrando o LeapMotion, feito na semana do dia 12/11

---
+++ SimpleTownCartoonAssets/Assets/MyWork/Script/SegWay/Andar.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Andar : MonoBehaviour {
+
+    public Rigidbody playerPrefab;
+
+    bool walkForward = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //moveFoward();
+    }
+
+    void FixedUpdate()
+    {
+        moveFoward();
+    }
+
+    public void stopFoward()
+    {
+        walkForward = false;
+    }
+
+    public void startFoward()
+    {
+        walkForward = true;
+    }
+
+    public void moveFoward()
+    {
+        if (walkForward)
+        {
+            playerPrefab.AddForce(new Vector3(0, 0, 40), ForceMode.Acceleration);
+        }
+    }
+
+    public void moveBackward()
+    {
+
+    }
+
+    public void rotateLeft()
+    {
+
+    }
+
+    public void rotateRight()
+    {
+
+    }
+}
--- SimpleTownCartoonAssets/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ SimpleTownCartoonAssets/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -125,7 +125,10 @@ namespace UnityStandardAssets.Characters.FirstPerson
             {
                 m_MoveDir += Physics.gravity*m_GravityMultiplier*Time.fixedDeltaTime;
             }
-            m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
+            
+            //MUDEI
+
+            //m_CollisionFlags = m_CharacterController.Move(m_MoveDir*Time.fixedDeltaTime);
 
             ProgressStepCycle(speed);
             UpdateCameraPosition(speed);
